90a3604f1e2a4eb252d607566f7125a7343cce45
Deepanshu Gupta <deepanshuguptacode@outlook.com>
2024-10-05 18:09:33 +0530

    Updated Flask app to bind to 0.0.0.0

--- app.py
+++ app.py
@@ -52,7 +52,8 @@ def get_gemini_response(text):
     return response.text
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=False)
+
 
 port = int(os.environ.get("PORT", 5000))
 app.run(host='0.0.0.0', port=port)
